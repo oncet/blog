d38a0ed49c71f2cc9ddff43a7fb5aa7060733aa0
runonce <camilorivera86@gmail.com>
2018-03-24 05:23:55 -0300

    Added date to post view

--- tests/Browser/ShowPostTest.php
+++ tests/Browser/ShowPostTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Browser;
 
-use App\Models\Post;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -12,18 +11,19 @@ class ShowPostTest extends DuskTestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function it_shows_post_title_and_body()
+    public function it_shows_post_title_body_and_date()
     {
-        Post::create([
+        $post = factory('App\Models\Post')->create([
             'title' => 'Foo',
             'body'  => '<p>Content.</p>'
         ]);
 
-        $this->browse(function ($browser) {
+        $this->browse(function ($browser) use($post) {
             $browser->visit('/posts/1')
-                    ->assertSee('Foo')
-                    ->assertSee('Content.')
-                    ->assertDontSee('<p>');
+                    ->assertSeeIn('h1', 'Foo')
+                    ->assertSeeIn('p', $post->created_at)
+                    ->assertDontSee('<p>')
+                    ->assertSee('Content.');
         });
     }
 }
