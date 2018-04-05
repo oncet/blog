87171ed9c789de31f1a2576958b0345b14863815
runonce <camilorivera86@gmail.com>
2018-04-05 19:54:12 -0300

    Add navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
     <title>@yield('title')</title>
   </head>
   <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a>
+    </nav>
     <div class="container mt-3">
       @yield('content')
     </div>
