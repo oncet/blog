0e128d3d7433ec23e13dd328be3f6e00817c874c
runonce <camilorivera86@gmail.com>
2018-04-06 14:33:06 -0300

    Changed HTML elements on posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.app')
 
-@section('title', 'Posts')
+@section('title', config('app.name'))
 
 @section('content')
 
-  @foreach ($posts as $post)
-    <div class="post">
-      <h2><a href="{{ route('post.show', ['slug' => $post->slug]) }}">{{ $post->title }}</a></h2>
-      <p>{{ $post->created_at->diffForHumans() }}</p>
-      @if ($post->cover)
-        <p><a href="{{ route('post.show', ['slug' => $post->slug]) }}"><img src="{{ route('imagecache', ['template' => 'large', 'filename' => $post->cover->file]) }}" alt="{{ $post->cover->alt }}"></a></p>
-      @endif
-      <div class="summary">
-        {!! $post->summary !!}
-      </div>
-    </div>
-  @endforeach
+    @foreach ($posts as $post)
+        <div class="post">
+            <div class="date" title="{{ $post->created_at }}">{{ $post->created_at->diffForHumans() }}</div>
 
-  {{ $posts->links() }}
+            <h2><a href="{{ route('post.show', ['slug' => $post->slug]) }}">{{ $post->title }}</a></h2>
+
+            @if ($post->cover)
+                <div class="mt-3 mb-3"><a href="{{ route('post.show', ['slug' => $post->slug]) }}"><img src="{{ route('imagecache', ['template' => 'large', 'filename' => $post->cover->file]) }}" alt="{{ $post->cover->alt }}"></a></div>
+            @endif
+
+            <div class="summary">
+                {!! $post->summary !!}
+            </div>
+
+            <hr>
+        </div>
+    @endforeach
+
+    {{ $posts->links() }}
 
 @endsection
