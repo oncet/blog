322efeefdcbc9e9fb03eb10cd1c81ce85d3fa1b5
runonce <camilorivera86@gmail.com>
2018-04-05 22:52:57 -0300

    Show app name instead of URL in link text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
   </head>
   <body>
     <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="{{ config('app.url') }}">{{ config('app.url') }}</a>
+      <a class="navbar-brand" href="{{ config('app.url') }}">{{ config('app.name') }}</a>
     </nav>
     <div class="container mt-3">
       @yield('content')
