3ceee3fbf60951a85c5d3300c8185f2963551432
runonce <camilorivera86@gmail.com>
2018-04-12 20:02:17 -0300

    Separate admin views

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+        <title>@yield('title') - {{ config('app.name') }} administration</title>
+
+        <style type="text/css">
+            img {
+                max-width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="{{ route('admin.post.index') }}">{{ config('app.name') }}</a>
+        </nav>
+        <div class="container mt-3">
+            @yield('content')
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+        @yield('scripts')
+    </body>
+</html>
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('admin.layouts.app')
 
 @section('title', 'Create a new post')
 
