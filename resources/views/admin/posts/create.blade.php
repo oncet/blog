8dbe5b32a533280500592acfc4de274592a9715b
runonce <camilorivera86@gmail.com>
2018-04-12 17:42:09 -0300

    Tiny cleanup

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,19 +5,23 @@
 @section('content')
 
     {!! Form::open(['route' => 'admin.post.store', 'files' => true]) !!}
+
         <div class="form-group">
             <label for="title">Title</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Enter a title">
         </div>
+
         <div class="form-group">
             <label for="image">Image</label>
             <input type="file" class="form-control-file" id="image" name="image">
         </div>
+
         <div class="form-group">
             <label for="body">Body</label>
             <textarea class="form-control" id="body" name="body" rows="10"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Create post</button>
+
     {!! Form::close() !!}
 
 @endsection
