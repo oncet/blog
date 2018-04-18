40526647a595ede91f003cfce3eebac2c4b8a291
runonce <camilorivera86@gmail.com>
2018-04-18 18:51:11 -0300

    Removed obsolete factory

--- database/factories/ImageFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Image::class, function (Faker $faker) {
-    return [
-        'file' => $faker->file(storage_path('samples'), public_path('img'), false)
-    ];
-});
