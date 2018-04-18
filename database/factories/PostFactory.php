7987aabdc67b3330ea6254337e55d335e343c74a
runonce <camilorivera86@gmail.com>
2018-04-18 18:51:30 -0300

    Updated post factory to include image file

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,6 +6,8 @@ $factory->define(App\Models\Post::class, function (Faker $faker) {
     return [
         'title' => $faker->word,
         'slug'  => str_slug($faker->word),
-        'body'  => '<p>' . $faker->text . '</p>'
+        'body'  => '<p>' . $faker->text . '</p>',
+        'image_file' => $faker->file(storage_path('/samples'), public_path('img'), false),
+        'image_alt'  => $faker->word
     ];
 });
