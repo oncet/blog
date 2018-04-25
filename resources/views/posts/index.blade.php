889c0e49a4de7e04defc5437f20ae55f0e7a273e
runonce <camilorivera86@gmail.com>
2018-04-25 18:01:15 -0300

    Added new column to index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,26 +4,41 @@
 
 @section('content')
 
-    @foreach ($posts as $post)
-        <div class="post">
-            <div class="date" title="{{ $post->created_at }}">{{ $post->created_at->diffForHumans() }}</div>
+    <div class="row">
 
-            <h2><a href="{{ route('post.show', ['slug' => $post->slug]) }}">{{ $post->title }}</a></h2>
+        <div class="col-9">
 
-            @if ($post->image_file)
-                <div class="mt-3 mb-3">
-                    <a href="{{ route('post.show', ['slug' => $post->slug]) }}"><img src="{{ $post->getImageSrc() }}" alt="{{ $post->image_alt }}"></a>
+            @foreach ($posts as $post)
+                <div class="post">
+                    <div class="date" title="{{ $post->created_at }}">{{ $post->created_at->diffForHumans() }}</div>
+
+                    <h2><a href="{{ route('post.show', ['slug' => $post->slug]) }}">{{ $post->title }}</a></h2>
+
+                    @if ($post->image_file)
+                        <div class="mt-3 mb-3">
+                            <a href="{{ route('post.show', ['slug' => $post->slug]) }}"><img src="{{ $post->getImageSrc() }}" alt="{{ $post->image_alt }}"></a>
+                        </div>
+                    @endif
+
+                    <div class="summary">
+                        {!! $post->summary !!}
+                    </div>
+
+                    <hr>
                 </div>
-            @endif
+            @endforeach
 
-            <div class="summary">
-                {!! $post->summary !!}
-            </div>
+            {{ $posts->links() }}
 
-            <hr>
         </div>
-    @endforeach
 
-    {{ $posts->links() }}
+        <div class="col">
+            <h4>About</h4>
+            <p>Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
+            <hr>
+            <h4>Tags</h4>
+            <p><a class="btn btn-light btn-sm" href="#" role="button">Printing</a> <a class="btn btn-light btn-sm" href="#" role="button">Textiles</a> <a class="btn btn-light btn-sm" href="#" role="button">Bookbinding</a></p>
+        </div>
+    </div>
 
 @endsection
